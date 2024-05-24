0291bccfc74f6ef24d95aab96f31c83e0cf9a407
Daulet Sergeev <95867411+Eit00@users.noreply.github.com>
2024-05-24 22:03:06 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Open the modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Close the modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function openModal1() {
+    document.getElementById("myModal1").style.display = "block";
+}
+
+// Close the modal
+function closeModal1() {
+    document.getElementById("myModal1").style.display = "none";
+}
+function openModal2() {
+    document.getElementById("myModal2").style.display = "block";
+}
+
+// Close the modal
+function closeModal2() {
+    document.getElementById("myModal2").style.display = "none";
+}
+function openModal3() {
+    document.getElementById("myModal3").style.display = "block";
+}
+
+// Close the modal
+function closeModal3() {
+    document.getElementById("myModal3").style.display = "none";
+}
